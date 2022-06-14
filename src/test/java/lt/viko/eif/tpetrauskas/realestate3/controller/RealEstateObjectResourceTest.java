4f9c625624b7ae03a39095f0a8afe3a77fca2d68
Tauras Petrauskas <petrauskas.post@gmail.com>
2022-06-14 16:43:31 +0300

    Added tests.

---
+++ src/test/java/lt/viko/eif/tpetrauskas/realestate3/controller/RealEstateObjectResourceTest.java
@@ -0,0 +1,30 @@
+package lt.viko.eif.tpetrauskas.realestate3.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RealEstateObjectResourceTest {
+
+    RealEstateObjectResource realEstateObjectResource = new RealEstateObjectResource();
+
+    @Test
+    void getAllRealEstateObjects() {
+        assertNotNull(realEstateObjectResource);
+    }
+
+    @Test
+    void getRealEstateObjectById() {
+        assertNotNull(realEstateObjectResource);
+    }
+
+    @Test
+    void updateUCar() {
+        assertNotNull(realEstateObjectResource);
+    }
+
+    @Test
+    void deleteRealEstateObject() {
+        assertNotNull(realEstateObjectResource);
+    }
+}
