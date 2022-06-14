2d30c9cb8c0ad62b66295e8e33738b6e75d22a04
Tauras Petrauskas <petrauskas.post@gmail.com>
2022-06-14 15:15:56 +0300

    Initial commit

---
+++ src/main/java/lt/viko/eif/tpetrauskas/realestate3/RealEstate3Application.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.tpetrauskas.realestate3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstate3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstate3Application.class, args);
+    }
+
+}
---
+++ src/test/java/lt/viko/eif/tpetrauskas/realestate3/RealEstate3ApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.tpetrauskas.realestate3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstate3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
